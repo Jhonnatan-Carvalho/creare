b6e57cabd5fde5ed73d12728b361dd88c8b57de8
TheGoldKay <jhonnatanwfc@live.com>
2024-03-17 19:50:15 -0300

    starting google sheets integration

--- creare_wdd/creare_wdd/settings.py
+++ creare_wdd/creare_wdd/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core', # the core of the application 
+    'gsheets', # the google sheets integration app
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GSHEETS = {
+    'CLIENT_SECRETS': 'credentials.json',
+}
---
+++ creare_wdd/start_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# type 'source start_env.sh' to start virtual environment
+source ~/creare/.venv/bin/activate
